fd1e1344ecd40d4d52d21fc05e563bf1d413de5d
Daniel Tyebkhan <dtyebkhan@gmail.com>
2021-12-02 13:59:41 -0800

    Added basic button app

--- App.tsx
+++ App.tsx
@@ -5,6 +5,11 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {NavigationContainer} from "@react-navigation/native";
+import {HomeScreen} from "./screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,10 +19,11 @@ export default function App() {
     return null;
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
 }
---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,22 @@
+import tailwind from "tailwind-rn";
+import {TouchableOpacity, View, Text} from "react-native";
+import {useState} from "react";
+import React from "react";
+
+export const HomeScreen = () => {
+    const [count, setCount] = useState(0);
+    return (
+        <View style={tailwind("bg-black flex justify-center items-center h-full")}>
+            <Text style={tailwind("text-white")}>{count}</Text>
+            <View style={tailwind('flex-row')}>
+                <TouchableOpacity style={tailwind('bg-blue-800 p-3 rounded-lg mt-5')} onPress={() => setCount(count-1)}>
+                    <Text style={tailwind('text-white')}>Decrease</Text>
+                </TouchableOpacity>
+                <View style={tailwind('m-2')}></View>
+                <TouchableOpacity style={tailwind('bg-blue-800 p-3 rounded-lg mt-5')} onPress={() => setCount(count+1)}>
+                    <Text style={tailwind('text-white')}>Increase</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}

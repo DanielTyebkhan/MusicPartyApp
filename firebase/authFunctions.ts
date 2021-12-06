8f3749829a22d40acb502bb58cf2a8598eb70c21
Daniel Tyebkhan <dtyebkhan@gmail.com>
2021-12-06 11:27:34 -0800

    Added dbExecutor

--- firebase/authFunctions.ts
+++ firebase/authFunctions.ts
@@ -3,33 +3,34 @@ import {db, fbAuth} from "./index";
 import {doc, setDoc} from "firebase/firestore";
 import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword} from "firebase/auth";
 
-export const fbSignUpUser = async (email: string, password: string): Promise<DbResponse> => {
+const dbExecutor = async (toExecute: () => Promise<DbSuccessResponse>): Promise<DbResponse> => {
   try {
-    const userCredential = await createUserWithEmailAndPassword(fbAuth, email, password);
-    return new DbSuccessResponse(userCredential);
+    return await toExecute();
   } catch (error) {
-    return new DbErrorResponse(error)
+    return new DbErrorResponse(error);
   }
 }
 
+export const fbSignUpUser = async (email: string, password: string): Promise<DbResponse> => {
+  return dbExecutor(async () => {
+    const userCredential = await createUserWithEmailAndPassword(fbAuth, email, password);
+    return new DbSuccessResponse(userCredential);
+  });
+}
+
 export const dbCreateUser = async (uuid: string, username: string): Promise<DbResponse> => {
-  try {
+  return dbExecutor(async () => {
     const writeResponse = await setDoc(doc(db, "users", uuid), {
       username
     });
     return new DbSuccessResponse(writeResponse);
-  } catch (error) {
-    return new DbErrorResponse(error);
-  }
+  });
 }
 
 export const dbLogInUser = async (email: string, password: string): Promise<DbResponse> => {
-  const auth = getAuth();
-  try {
+  return dbExecutor(async() => {
+    const auth = getAuth();
     const signInResponse = await signInWithEmailAndPassword(auth, email, password);
     return new DbSuccessResponse(signInResponse);
-  }
-  catch (error) {
-    return new DbErrorResponse(error);
-  }
+  });
 }

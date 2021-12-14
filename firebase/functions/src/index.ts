06bc9eacc473147bf2edb9307a5549c3a80b4f9b
Daniel Tyebkhan <dtyebkhan@gmail.com>
2021-12-13 16:54:47 -0800

    Wrote function to fetch spotify accesstoken

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,13 +1,32 @@
 import * as functions from "firebase-functions";
+import axios from "axios";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+require('dotenv').config()
 
-export const spotifySignIn = functions.https.onRequest((request, response) => {
+export const getSpotifyAccessToken = functions.https.onRequest(async (request, response) => {
+  const {userId, spotifyCode, redirectUri} = request.params;
+  const auth = new Buffer(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString('base64')
+  const headers = {
+    Authorization: `Basic ${auth}`,
+    'Content-Type': 'application/x-www-form-urlencoded'
+  };
+  const params = {
+    grant_type: "authorization_code",
+    code: spotifyCode,
+    redirect_uri: redirectUri
+  };
+  const config = {
+    headers,
+    params
+  }
+  const accessTokenResponse = await axios.get('https://accounts.spotify.com/api/token', config);
+  // TODO: store access token in db
+  if (accessTokenResponse.status === 200)
+    response.send({success: true});
+  else
+    response.send({success: false})
+});
 
+export const refreshSpotifyAccessToken = functions.https.onRequest((request, response) => {
+  response.send('not yet implemented');
 });

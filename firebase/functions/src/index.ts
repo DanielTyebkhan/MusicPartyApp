6594792c0d99434e5cc5601cb16f046458336000
Daniel Tyebkhan <dtyebkhan@gmail.com>
2021-12-15 18:44:24 -0800

    Added firebase emulators setup

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,7 +1,10 @@
 import * as functions from "firebase-functions";
 import axios from "axios";
+import {initializeApp} from "firebase-admin";
 
 require('dotenv').config()
+const app = initializeApp();
+const firestore = app.firestore();
 
 export const getSpotifyAccessToken = functions.https.onRequest(async (request, response) => {
   const {userId, spotifyCode, redirectUri} = request.params;
@@ -24,9 +27,22 @@ export const getSpotifyAccessToken = functions.https.onRequest(async (request, r
     response.send({success: false, message: "failed to generate access token"});
     return;
   }
-
+  const written = await writeAccessToken(userId, 'spotify', accessTokenResponse.data.access_token);
+  if (!written) {
+    response.send({success: false, message: "failed to save access token"})
+  }
   response.send({success: true});
 });
+
+const writeAccessToken = async (uid: string, type: string, token: string) => {
+  const tokenRef = firestore.doc(`users/${uid}/tokens`);
+  try {
+    await tokenRef.set({type: token}, {merge: true});
+  } catch (error) {
+    return false;
+  }
+  return true;
+};
 
 export const refreshSpotifyAccessToken = functions.https.onRequest((request, response) => {
   response.send('not yet implemented');
